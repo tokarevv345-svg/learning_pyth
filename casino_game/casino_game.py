e123ebda6abb6c80a2911a8cce2b69c2375113a1
tokarevv345-svg <tokarevv345@gmail.com>
2026-08-24 00:37:06 +0300

    закончил казик

--- casino_game/casino_game.py
+++ casino_game/casino_game.py
@@ -1,18 +1,41 @@
+# import random
+# import colorama
+# slots = ["🍒", "🍇", 7]
+# positions = ["🍒", "🍇", 7]
+# win = False
+# attempt = 0
+# print("нажмите \"Enter\" чтобы крутить рулетку ", end = "")
+# while win != True:
+#     input()
+#     attempt += 1
+#     slots = random.choices(positions, k=3)
+#     print(slots)
+#     if slots[0] == slots[1] == slots[2]:
+#         print("вы победили!")
+#         print(f"ваше кол-во попыток: {attempt}")
+#         win = True
+#     else:
+#         win = False
+
+#---------------------------------------------------------------------------------
+
 import random
-import colorama
+from colorama import Fore, Style
+EndColor = Style.RESET_ALL
 slots = ["🍒", "🍇", 7]
 positions = ["🍒", "🍇", 7]
 win = False
 attempt = 0
-print("нажмите \"Enter\" чтобы крутить рулетку ", end = "")
+print(Fore.YELLOW + "нажмите \"Enter\" чтобы крутить рулетку ", end = "" + EndColor)
 while win != True:
     input()
     attempt += 1
     slots = random.choices(positions, k=3)
     print(slots)
-    if slots[0] == slots[1] == slots[2]:
-        print("вы победили!")
+    if len(set(slots)) == 1:
+        print(Fore.GREEN + "вы победили!" + EndColor)
         print(f"ваше кол-во попыток: {attempt}")
         win = True
     else:
         win = False
+
