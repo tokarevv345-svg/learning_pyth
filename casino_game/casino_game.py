9cf9dec48a17bd47e2d85e28d63d4ca84decd777
tokarevv345-svg <tokarevv345@gmail.com>
2026-08-24 00:07:56 +0300

    казик

---
+++ casino_game/casino_game.py
@@ -0,0 +1,18 @@
+import random
+import colorama
+slots = ["🍒", "🍇", 7]
+positions = ["🍒", "🍇", 7]
+win = False
+attempt = 0
+print("нажмите \"Enter\" чтобы крутить рулетку ", end = "")
+while win != True:
+    input()
+    attempt += 1
+    slots = random.choices(positions, k=3)
+    print(slots)
+    if slots[0] == slots[1] == slots[2]:
+        print("вы победили!")
+        print(f"ваше кол-во попыток: {attempt}")
+        win = True
+    else:
+        win = False
